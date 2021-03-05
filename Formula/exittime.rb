55e4edb81321bb78057184ca6fc8f48eac3351ef
Ricardo Calixto Duque <ricardocalixtoduque@gmail.com>
2021-03-05 11:44:42 -0300

    exittime version 0.0.2

--- Formula/exittime.rb
+++ Formula/exittime.rb
@@ -1,8 +1,8 @@
 class Exittime < Formula
   desc "Calculate the exit time"
   homepage "https://github.com/RicDuque/exittime"
-  url "https://github.com/RicDuque/exittime/archive/0.0.1.tar.gz"
-  sha256 "ee4541524365d00ec000b81eeaddba4f430d244738f7e8b88e8f3758e885bb42"
+  url "https://github.com/RicDuque/exittime/archive/0.0.2.tar.gz"
+  sha256 "36d264518da69e3062743ed1f3eca67dba1639b1fafd3d1904289397518e55d2"
   license ""
 
   def install
