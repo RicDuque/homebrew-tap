3bd1e1f058ea2c7da11c5a3b7767a1f8ba6c3694
Ricardo Calixto Duque <ricardocalixtoduque@gmail.com>
2021-03-16 21:19:28 -0300

    fixing hash

--- Formula/exittime.rb
+++ Formula/exittime.rb
@@ -2,7 +2,7 @@ class Exittime < Formula
   desc "Calculate the exit time"
   homepage "https://github.com/RicDuque/exittime"
   url "https://github.com/RicDuque/exittime/archive/0.0.4.tar.gz"
-  sha256 "2bd0becb32fb7af9ed69f4e39c8abcd533ec396ed9df91f063ad51f133fe83ed"
+  sha256 "360f601559a9ae9e6627c47e520777500075b91295e1512cf650d1bfd0f97ff1"
   license ""
 
   def install
